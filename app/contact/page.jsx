f6e5238f5cff0c333fd1c78376aead7e25da7c0e
Musaddek-Ahmed <musaddek59@gmail.com>
2024-05-25 03:56:51 +0600

    updated 	modified:   app/contact/page.jsx 	modified:   app/work/page.jsx 	modified:   components/ui/input.jsx 	new file:   components/ui/sliderBtns.jsx 	modified:   components/ui/textarea.jsx 	modified:   package-lock.json 	modified:   package.json 	new file:   public/assets/projects/project1.png 	new file:   public/assets/projects/project2.png 	new file:   public/assets/projects/project3.png 	new file:   public/assets/projects/project4.png

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,83 @@
+'use client'
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa'
+import { Description } from '@radix-ui/react-dialog'
+import {motion} from 'framer-motion'
+
+const info = [
+  {
+    icon: <FaPhoneAlt/>,
+    title: 'Phone',
+    description: '(+880) 1820-896472'
+  },
+  {
+    icon: <FaEnvelope/>,
+    title: 'Email',
+    description: 'musaddek59@gmail.com'
+  },
+  {
+    icon: <FaMapMarkerAlt/>,
+    title: 'Address',
+    description: 'Uttar Kazitula, Electric Supply Road, Sylhet'
+  },
+]
 
 const Contact = () => {
   return (
-    <div>Contact page</div>
+    <motion.section
+     initial={{opacity: 0}}
+     animate={{
+      opacity: 1,
+      transition: {delay:2.2, duration:0.4, ease: 'easeIn'}
+     }}
+     className='py-6'
+     >
+      <div className='container mx-auto'>
+        <div className='flex flex-col xl:flex-row gap-[30px]'>
+          <div className='xl:h-[54%] order-2 xl:order-none'>
+            <form className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'>
+              <h3 className='text-4xl text-accent'>Let's work together</h3>
+
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                <Input type='name' placeholder='Name'/>
+                <Input type='email' placeholder='Email Address'/>
+                <Input type='phone' placeholder='Phone Number'/>
+              </div>
+              <Textarea
+               className='h-[200px]'
+               placeholder='Type your message here...'
+              />
+              <Button size='md' className='max-w-40'>Send message</Button>
+            </form>
+          </div>
+
+          <div className='flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0'>
+            <ul className='flex flex-col gap-10'>
+              {info.map((item, index) => {
+                return(
+                  <li key={index} className='flex items-center gap-6'>
+                      <div className='w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center'>
+                        <div className='text-[28px]'>{item.icon}</div>
+
+                      </div>
+                      <div>
+                        <p>{item.title}</p>
+                        <h3>{item.description}</h3>
+                      </div>
+                  </li>
+                )
+              })}
+            </ul>
+            </div>
+        </div>
+
+      </div>
+    </motion.section>
   )
 }
 
--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,8 +1,156 @@
+'use client'
 import React from 'react'
+import {motion} from 'framer-motion'
+import { useState } from 'react'
+import { Swiper, SwiperSlide} from 'swiper/react' 
+import 'swiper/css'
+import {BsArrowUpRight, BsGithub} from 'react-icons/bs'
+
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+import Link from 'next/link'
+import Image from 'next/image'
+import SliderBtns from '@/components/ui/sliderBtns'
+
+const projects = [
+  {
+    num: '01',
+    category: 'fullstack',
+    title: 'project 1',
+    description: 'a social media website where user can post, delete and download images of high quality and search there choiceful category pictures.',
+    stack: [{name: 'React'}, {name: 'TailwindCss'}, {name: 'Sanity'}],
+    image: '/assets/projects/project2.png',
+    live: 'https://lookme-social-media.netlify.app/login',
+    github: 'https://github.com/Musaddek-Ahmed/lookme_frontend.git'
+  },
+  {
+    num: '02',
+    category: 'frontend',
+    title: 'project 2',
+    description: 'I made this website for a company. I used HTML, CSS and Javascript here. Also used GSAP for animation. Complete frontend was developed by me.',
+    stack: [{name: 'Html 5'}, {name: 'Css 3'}, {name: 'Javascript'}],
+    image: '/assets/projects/project1.png',
+    live: 'https://www.vercardo.com',
+    github: 'https://github.com/Musaddek-Ahmed/vercardo.git'
+  },
+  
+  {
+    num: '03',
+    category: 'frontend',
+    title: 'project 3',
+    description: 'A beautiful homepage with core implementation of GSAP and minor animations to attract the audience.',
+    stack: [{name: 'Html 5'}, {name: 'Css 3'}, {name: 'Javascript'}],
+    image: '/assets/projects/project3.png',
+    live: 'https://musaddek-ahmed.github.io/two-good/',
+    github: 'https://github.com/Musaddek-Ahmed/Two-Good.git'
+  },
+
+]
 
 const Work = () => {
+  const [project, setProject] = useState(projects[0])
+
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  }
   return (
-    <div>Work page</div>
+    <motion.section
+     initial={{opacity: 0}}
+     animate={{
+      opacity: 1,
+      transition: {delay:2.2, duration:0.4, ease: 'easeIn'}
+     }}
+     className='min-h-[80vh] flex flex-col justify-center py-12 xl:px-0'
+    >
+      <div className='container mx-auto'>
+        <div className='flex flex-col xl:flex-row xl:gap-[30px]'>
+          <div className='w-full xl:w-[50%] xl:h-[400px] flex flex-col xl:justify-between order-2 xl:order-none'>
+            <div className='flex flex-col gap-[30px]'>
+
+              <div className='text-6xl text-white font-extrabold text-transparent text-outline'>
+                {project.num}
+              </div>
+              <h2 className='text-[38px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize'>
+                {project.category} project
+              </h2>
+              <p className='text-white/60'>{project.description}</p>
+              <ul className='flex gap-4'>
+                {project.stack.map((item, index) => {
+                  return(
+                    <li key={index} className='text-xl text-accent'>
+                        {item.name}
+                        {index !== project.stack.length - 1 && ","}
+                    </li>
+                  )
+                })}
+              </ul>
+              <div className='border border-white/20'></div>
+              <div className='flex items-center gap-4'>
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className='w-[50px] h-[50px] rounded-full bg-white/5 flex justify-center items-center group'>
+                        <BsArrowUpRight className='text-white text-2xl group-hover:text-accent'/>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className='w-[50px] h-[50px] rounded-full bg-white/5 flex justify-center items-center group'>
+                        <BsGithub className='text-white text-2xl group-hover:text-accent'/>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+
+            </div>
+
+          </div>
+          <div className='w-full xl:w-[50%]'>
+            <Swiper 
+             spaceBetween={30}
+             slidesPerView={1}
+             className='xl:h-[520px] mb-12'
+             onSlideChange={handleSlideChange}
+            >
+                {projects.map((project, index) => {
+                  return (
+                    <SwiperSlide key={index} className='w-full'>
+                        <div className='h-[400px] relative group flex justify-center items-center bg-pink-50/20'>
+                          <div></div>
+
+                          <div className='relative w-full h-full'>
+                            <Image 
+                              src={project.image}
+                              fill
+                              className='object-cover'
+                              alt='image'
+                            />
+                          </div>
+                        </div>
+                    </SwiperSlide>
+                  )
+                })}
+
+                <SliderBtns 
+                 containerStyles='flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none'
+                 btnStyles='bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all'
+                />
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
   )
 }
 

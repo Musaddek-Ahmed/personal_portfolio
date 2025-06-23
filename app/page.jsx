6b63acc983ccea0f68710c189b8b7e704be097e0
Musaddek-Ahmed <musaddek59@gmail.com>
2025-06-23 13:24:43 +0600

    latest update

--- app/page.jsx
+++ app/page.jsx
@@ -9,12 +9,12 @@ const Home = () => {
     <section className='h-full'>
       <div className='container mx-auto h-full'>
         <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-0 xl:pb-24'>
-          <div className='text-center xl:text-left order-2 xl:order-none mt-8 '>
+          <div className='text-center xl:text-left order-2 xl:order-none mt-5 '>
             <span className='text-xl'>Frontend Developer</span>
             <h1 className='h1 mb-6 '>
               Hello I'm <br /> <span className=' text-accent'>Musaddek Ahmed</span>
             </h1>
-            <p className='max-w-[500px] mb-9 text-white/80'>I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies</p>
+            <p className='max-w-[500px] mb-6 text-white/80'>Passionate about building polished, high-performance web experiences with a strong grasp of modern technologies.</p>
             <div className='flex flex-col xl:flex-row items-center gap-8'>
               <a href="/Resumee.pdf" download>
               <Button
@@ -37,7 +37,7 @@ const Home = () => {
                
             </div>
 
-            <p className=' text-slate-400 mt-10 text-sm mb-10'>© {new Date().getFullYear()} Musaddek Ahmed. All rights reserved.</p>
+            <p className=' text-slate-400 mt-10 text-sm mb-5'>© {new Date().getFullYear()} Musaddek Ahmed. All rights reserved.</p>
             
           </div>
           <div className='order-1 xl:order-none mb-8 xl:mb-0'>

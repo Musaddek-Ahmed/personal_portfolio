d135ab5b7bbe18372b09221dd4cf656ffc6ed4f7
Musaddek-Ahmed <musaddek59@gmail.com>
2025-08-12 14:28:24 +0600

    latest

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -161,10 +161,13 @@ const Work = () => {
               <div className='text-6xl text-white font-extrabold text-transparent text-outline'>
                 {project.num}
               </div>
-              <h2 className='text-[38px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize'>
+              <div className='flex flex-col'>
+                <h2 className='text-[38px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize'>
                 {project.title}
               </h2>
-              <p className='text-white'>{project.category}</p>
+              <p className='text-white'>{project.category} project</p>
+              </div>
+              
               <p className='text-white/60'>{project.description}</p>
               <ul className='flex gap-4'>
                 {project.stack.map((item, index) => {
